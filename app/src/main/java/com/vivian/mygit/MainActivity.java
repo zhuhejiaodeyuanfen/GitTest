2b9a03d688089ab8af51ee1b3a4eb846ea484190
vivianwq <522029704@qq.com>
2018-06-03 22:10:05 +0800

    提交更新到runs分支

--- app/src/main/java/com/vivian/mygit/MainActivity.java
+++ app/src/main/java/com/vivian/mygit/MainActivity.java
@@ -7,10 +7,26 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        new GitA();
+        new GitB();
+        new GitC();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("我是王亚军 我也要修改东西");
         new TestA();
+        get();
         new TestB();
+        println(ID_GET);
     }
+
+    private void get() {
+        System.out.println("我是王亚军 我也要修改东西");
+    }
+
+
+    private void println(String str) {
+        System.out.println("我是王亚军 我也要修改东西" + str);
+    }
+
+    public static final String ID_GET = "222";
 }
