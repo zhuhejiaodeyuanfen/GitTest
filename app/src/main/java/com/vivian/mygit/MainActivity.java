14ae5a5920527aebf531522ad4d669f23edb41e0
vivianwq <522029704@qq.com>
2018-06-03 21:53:17 +0800

    我要提交到runs_的分支

--- app/src/main/java/com/vivian/mygit/MainActivity.java
+++ app/src/main/java/com/vivian/mygit/MainActivity.java
@@ -3,11 +3,16 @@ package com.vivian.mygit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import junit.framework.Test;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("我是王亚军 我也要修改东西");
+        new TestA();
+        new TestB();
     }
 }
